392813aaee40b18a2ee754bc716b954c45cda440
Bonface <bonniewanga34@gmail.com>
2023-10-02 18:28:03 +0300

    Completed assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,11 @@
-# your code goes here!
+def countdown(seconds):
+    while seconds > 0:
+        print(f'{seconds} SECOND(S)!')
+        seconds -= 1        
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(seconds):
+    while seconds > 0:
+        print(f'{seconds} SECOND(S)!')
+        seconds -= 1        
+    print('HAPPY NEW YEAR!')
